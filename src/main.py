69b33ed7afe8371b27edb4ecb8370ce1f27891bd
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-26 00:04:51 +0100

    feat(main.py): Ex2 Resolution by GOIS

--- src/main.py
+++ src/main.py
@@ -1,12 +1,56 @@
 #!/usr/bin/env python3
 
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ != "__main__":
     exit(0)
 
+def scatter(
+        values, varNames,
+        *,
+        title: str | None = None,
+        xLabel: str,
+        yLabel: str,
+        color: str = 'purple',
+        marker: str = 'o',
+        grid: bool = True,
+        figure: Figure,
+        subplot: int,
+        ):
+    if title is None:
+        title = f'{yLabel} vs {xLabel}'
+
+    plt.figure(figure)
+    plt.subplot(subplot)
+    xValues = values[:, varNames.index(xLabel)]
+    yValues = values[:, varNames.index(yLabel)]
+    plt.scatter(xValues, yValues, color = color, marker = marker)
+    plt.title(title)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.grid(grid)
+
 data = pd.read_excel('./assets/CarDataset.xlsx')
 varNames = data.columns.values.tolist()
 values = data.values
-print(varNames)
-print(values)
+
+figure, _ = plt.subplots(nrows = 3, ncols = 2)
+figure.suptitle('Ex2')
+figure.subplots_adjust(wspace=0.5, hspace=0.5)
+variablePairs = (
+        ('Acceleration', 'MPG'),
+        ('Cylinders', 'MPG'),
+        ('Displacement', 'MPG'),
+        ('Horsepower', 'MPG'),
+        ('ModelYear', 'MPG'),
+        ('Weight', 'MPG'),
+        )
+for index, (xLabel, yLabel) in enumerate(variablePairs):
+    # TODO: adjust subplot size according to variablePairs size
+    scatter(values, varNames, xLabel = xLabel, yLabel = yLabel,
+            figure = figure,
+            subplot = int(f'32{index+1}'))
+
+plt.show()
