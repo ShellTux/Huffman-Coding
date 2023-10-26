6b9f34871f779a2d5fbf99248aa485593cbb2f88
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-26 14:25:16 +0100

    refactor: move variableAlphabet function -> `data.py`

--- src/data.py
+++ src/data.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from numpy import ndarray
+from numpy._typing import NDArray
+import numpy as np
+import pandas as pd
 
 data = pd.read_excel('./assets/CarDataset.xlsx')
 varNames: list[str] = data.columns.values.tolist()
@@ -12,3 +14,14 @@ def variableValues(
         label: str,
         ) -> ndarray:
     return values[:, varNames.index(label)]
+
+
+def variableAlphabet(
+        values: ndarray,
+        varNames: list[str],
+        *,
+        label: str
+        ) -> NDArray[np.uint16]:
+    labelValues = variableValues(values, varNames, label = label)
+
+    return np.unique(labelValues).astype(np.uint16)
--- src/ex3.py
+++ src/ex3.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python3
 
-from data import values, varNames, variableValues
-from numpy import ndarray
-from numpy.typing import NDArray
-import numpy as np
-
-def variableAlphabet(
-        values: ndarray,
-        varNames: list[str],
-        *,
-        label: str
-        ) -> NDArray[np.uint16]:
-    labelValues = variableValues(values, varNames, label = label)
-
-    return np.unique(labelValues).astype(np.uint16)
+from data import values, varNames, variableAlphabet
 
 if __name__ == "__main__":
     for label in varNames:
