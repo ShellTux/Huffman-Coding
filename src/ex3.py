45e1d768b227b95fdcd9af2c28b3ccbb8f67975d
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-26 11:15:01 +0100

    Ex3 Resolution by GOIS

--- src/ex3.py
+++ src/ex3.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
+from numpy import ndarray
+from numpy.typing import NDArray
+import numpy as np
 import pandas as pd
 
 if __name__ != "__main__":
     exit(0)
 
+def variableAlphabet(
+        values: ndarray,
+        varNames: list[str],
+        *,
+        label: str
+        ) -> NDArray[np.uint16]:
+    labelValues = values[:, varNames.index(label)]
+
+    return np.unique(labelValues).astype(np.uint16)
+
 data = pd.read_excel('./assets/CarDataset.xlsx')
 varNames = data.columns.values.tolist()
 values = data.values
+
+for label in varNames:
+    print(variableAlphabet(values, varNames, label = label))
