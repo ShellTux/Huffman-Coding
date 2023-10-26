1ed60a085544a0be54fbad20acfee772079ea2d7
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-26 13:41:02 +0100

    feat(src/ex3.py): export variableAlphabet
    variableAlphabet is a function that returns the alphabet of a data
    source

--- src/ex3.py
+++ src/ex3.py
@@ -5,9 +5,6 @@ from numpy.typing import NDArray
 import numpy as np
 from data import values, varNames, variableValues
 
-if __name__ != "__main__":
-    exit(0)
-
 def variableAlphabet(
         values: ndarray,
         varNames: list[str],
@@ -18,5 +15,9 @@ def variableAlphabet(
 
     return np.unique(labelValues).astype(np.uint16)
 
-for label in varNames:
-    print(variableAlphabet(values, varNames, label = label))
+if __name__ == "__main__":
+    for label in varNames:
+        print(f'{label}:', variableAlphabet(values, varNames, label = label),
+              sep = '\n',
+              end = '\n\n',
+              )
