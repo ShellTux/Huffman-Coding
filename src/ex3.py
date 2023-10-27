1869d2d5f5d480c617dd24a559a20373170d8bcc
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-27 04:03:30 +0100

    Move from Imperitive to Object Oriented
    Improve readability

--- src/ex3.py
+++ src/ex3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from data import values, varNames, variableAlphabet
+from data import DATA
 
 if __name__ == "__main__":
-    for label in varNames:
-        print(f'{label}:', variableAlphabet(values, varNames, label = label),
+    for variable in DATA.getVariables():
+        print(f'{variable}:', DATA.getAlphabet(variable = variable),
               sep = '\n',
               end = '\n\n',
               )
--- src/ex5.py
+++ src/ex5.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
-from data import values, variableAlphabet, variableValues, varNames
-from ex4 import alphabetOccurrenceCount
+from data import DATA
 from graph import histogram
 import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
     figure = plt.figure('Histogram')
-    label = 'Acceleration'
-    accelerationValues = variableValues(values, varNames, label =
-                                        label).astype(np.uint16)
-    accelerationAlphabet = variableAlphabet(values, varNames, label = label)
-    accelerationAlphabetOccurrence = alphabetOccurrenceCount(accelerationValues)
-    histogram(
-            accelerationAlphabet,
-            accelerationAlphabetOccurrence,
-            xLabel = 'Acceleration',
-            yLabel = 'Count',
-            figure = figure
+    variable = 'Acceleration'
+    values = DATA.getValues(variable = variable).astype(np.uint16)
+    alphabet, alphabetCount = DATA.getAlphabet(
+            variable    = variable,
+            returnCount = True
             )
-    plt.savefig('Ex5.png')
+    print(alphabet, alphabetCount, sep = '\n')
+    histogram(
+            xValues = alphabet,
+            yValues = alphabetCount,
+            xLabel  = variable,
+            yLabel  = 'Count',
+            figure  = figure,
+            )
+
     plt.show()
+
+    figure.subplots_adjust(wspace = .5, hspace = .5)
+    figure.savefig('Ex5.png')
