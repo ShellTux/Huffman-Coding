1ec2e5ec448a641d58f9f5542ddf1058f9ad090a
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-10-27 12:28:52 +0100

    refactor(ex7): add padding to print statements

--- src/ex7.py
+++ src/ex7.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 from data import DATA
+import numpy as np
 
 if __name__ == "__main__":
-    for variable in DATA.getVariables():
+    variables = DATA.getVariables()
+    maxVariableLength = max(map(len, variables))
+    for variable in variables:
         values = DATA.getValues(variable = variable)
         bps = DATA.bitsPerSymbol(variable = variable)
-        print(f'{variable} = {bps:.2f} bits/symbol')
+        print(f'{variable:<{maxVariableLength}} = {bps:.2f} bits/symbol')
 
     print()
 
     # All values
-    valuesFlatten = DATA.getValues().flatten()
     valuesBPS = DATA.bitsPerSymbol()
     print(f'bps(values) = {valuesBPS:.2f} bits/symbol')
