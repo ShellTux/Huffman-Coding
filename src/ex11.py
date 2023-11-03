80b78b61fdfe9e00570e1eb2a4f73d72e679150f
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-03 03:07:42 +0000

    chore(main.py): import `ex9` and `ex11` to main script

--- src/ex11.py
+++ src/ex11.py
@@ -1,7 +1,4 @@
 from data import DATA
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from numpy._typing import NDArray
 
 sliceLength = 10
--- src/ex9.py
+++ src/ex9.py
@@ -1,9 +1,10 @@
 from data import DATA
-import numpy as np
 
-if __name__ == "__main__":
-    
+def main():
     varNames = DATA.getVariables()
     varValues = DATA.getValues()
     for index in range(varValues.shape[1] - 1):
         print(f"Coeficiencia de {varNames[6]} e {varNames[index]}: {DATA.pearsonCoeficient(varValues[:, 6], varValues[:,index])}")
+
+if __name__ == "__main__":
+    main()
