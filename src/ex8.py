65fd6440d391b05bd46e32ff7260d5ad944636ac
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-03 02:24:33 +0000

    chore(src/ex8.py): print average bps and variance length for all values

--- src/ex8.py
+++ src/ex8.py
@@ -15,3 +15,14 @@ def main():
                 sep = '\n',
                 end = '\n' * 2
                 )
+
+    avgBits = DATA.averageBitsPerSymbol()
+    lengthVariance = DATA.lengthVariance()
+    print(
+            'All',
+            f'{f"Average bits per symbol:":<{24}} {avgBits:.3f}',
+            f'{f"Variance of lengths:":<{24}} {lengthVariance:.3f}',
+            sep = '\n',
+            end = '\n' * 2
+            )
+
