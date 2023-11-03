3a9aa085d0dcda84600dc54a29fa69e7e45d0a66
Luís Góis <115948079+ShellTux@users.noreply.github.com>
2023-11-03 17:00:06 +0000

    feat: run exercises through command line (#17)
    Example:
    ```shell
    $ python main.py 4 8
    ```

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from src.ex10 import main as ex10
+from src.ex11 import main as ex11
 from src.ex2  import main as ex2
 from src.ex3  import main as ex3
 from src.ex4  import main as ex4
@@ -8,28 +10,36 @@ from src.ex6  import main as ex6
 from src.ex7  import main as ex7
 from src.ex8  import main as ex8
 from src.ex9  import main as ex9
-from src.ex10 import main as ex10
-from src.ex11 import main as ex11
-
-GREEN = '\033[32m'
-RESET = '\033[0m'
+from colors import GREEN, RESET
+import sys
 
 if __name__ == "__main__":
     exercices = (
-            ('Ex2', ex2),
-            ('Ex3', ex3),
-            ('Ex4', ex4),
-            ('Ex5', ex5),
-            ('Ex6', ex6),
-            ('Ex7', ex7),
-            ('Ex8', ex8),
-            ('Ex9', ex9),
-            ('Ex10', ex10),
-            ('Ex11', ex11),
+            (2,  ex2),
+            (3,  ex3),
+            (4,  ex4),
+            (5,  ex5),
+            (6,  ex6),
+            (7,  ex7),
+            (8,  ex8),
+            (9,  ex9),
+            (10, ex10),
+            (11, ex11),
             )
 
-    for name, main in exercices:
+    arguments = sys.argv[1:]
+    exercises = []
+    for index, argument in enumerate(arguments):
+        try:
+            exercises.append(int(argument))
+        except ValueError:
+            print(f"Error: '{argument}' is not a valid number.")
+
+    for number, main in exercices:
+        if len(exercises) != 0 and number not in exercises:
+            continue
+
         print()
-        print(GREEN + f'-------{name}-------' + '\033[0m')
+        print(GREEN + f'-------Ex{number}-------' + RESET)
         main()
         print()
