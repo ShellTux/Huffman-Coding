6231b583f039cdf8054363b36d2eaf9da108894c
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-03 04:04:35 +0000

    finish and add ex10 to main.py

--- src/ex10.py
+++ src/ex10.py
@@ -2,11 +2,16 @@ from data import DATA
 
 
 def main():
-    MI = DATA.mutualInformation(
-            variableX = 'MPG',
-            variableY = 'Weight',
+    variables = (
+            ('Acceleration', 'MPG'),
+            ('Weight', 'MPG'),
             )
-    print(MI)
+    for variableX, variableY in variables:
+        MI = DATA.mutualInformation(
+                variableX = variableX,
+                variableY = variableY,
+                )
+        print(f'Mutual Information ({variableX}, {variableY}) = {MI}')
 
 if __name__ == "__main__":
     main()
