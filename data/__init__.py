583072af8bbb4a98cf0e1a0411f207348850e897
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-04 22:25:40 +0000

    fix: convert values to uint16

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,7 @@
 from .data import Data
+from numpy import uint16
 import os
 
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 file_path = os.path.join(root, 'assets', 'CarDataset.xlsx')
-DATA = Data(file_path)
+DATA = Data(file_path, dataType = uint16)
